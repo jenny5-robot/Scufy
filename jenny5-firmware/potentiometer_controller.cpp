4dffe733f404325e37947a4eef053ce6efb97432
mihaioltean <moltean@cs.ubbcluj.ro>
2016-01-22 13:07:22 +0200

    parameters of each potentiometer are encapsulated in this class

---
+++ jenny5-firmware/potentiometer_controller.cpp
@@ -0,0 +1,39 @@
+#include "potentiometer_controller.h"
+#include "Arduino.h"
+//--------------------------------------------------------------------
+t_potentiometer_controller::t_potentiometer_controller(void)
+{
+  pin = 2;
+  low = 2;
+  high = 2;
+  _home = 2;
+}
+//--------------------------------------------------------------------
+void t_potentiometer_controller::set_params (byte _pin, int _low, int _high, int __home)
+{
+		pin = _pin;
+		low = _low;
+   high = _high;
+   _home = __home;
+}
+//--------------------------------------------------------------------
+int	t_potentiometer_controller::get_position(void)
+{
+	return (int)analogRead(pin);
+}
+//--------------------------------------------------------------------
+void t_potentiometer_controller::get_params(byte *_pin, int *_low, int *_high, int *__home)
+{
+	*_low = low;
+  *_high = high;
+  *__home = _home;
+  *_pin = pin;
+}
+//--------------------------------------------------------------------
+int t_potentiometer_controller::is_within_limits(void)
+{
+	int val = get_position();
+
+	return low <= val && val <= high;
+}
+//--------------------------------------------------------------------
