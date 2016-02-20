0603c0d023c6be060161fe95aa0ec4499bb1f76c
mihaioltean <moltean@cs.ubbcluj.ro>
2016-02-20 13:55:43 +0200

    added types for motor's state

--- jenny5-firmware/jenny5_types.h
+++ jenny5-firmware/jenny5_types.h
@@ -5,6 +5,10 @@
 #define BUTTON 0
 #define POTENTIOMETER 1
 #define ULTRASOUND 2
+
+#define MOTOR_JUST_STOPED 0
+#define MOTOR_STILL_RUNNING 1
+#define MOTOR_DOES_NOTHING 2
 //---------------------------------------------------
 
 struct t_sensor_info {
