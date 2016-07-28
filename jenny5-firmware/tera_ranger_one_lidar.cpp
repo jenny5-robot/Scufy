f76cf2b5dca167ce0cb7642177623ea3f565f57a
mihaioltean <moltean@cs.ubbcluj.ro>
2016-07-28 14:18:38 +0300

    added tera ranger LIDAR
    not tested yet.

--- jenny5-firmware/tera_ranger_one_lidar.cpp
+++ jenny5-firmware/tera_ranger_one_lidar.cpp
@@ -1,29 +1,83 @@
 #include "tera_ranger_one_lidar.h"
 
-  //-----------------------------------------------------------------------------
-  t_tera_ranger_one_lidar::t_tera_ranger_one_lidar(byte motor_dir_pin, byte motor_step_pin, byte motor_enable_pin, byte ir_pin)
-  {
-    started = false;
-    infrared_read_first_time = false;
-  }
-  //-----------------------------------------------------------------------------
-  void t_tera_ranger_one_lidar::get_params(byte *motor_dir_pin, byte *motor_step_pin, byte *motor_enable_pin, byte *ir_pin)
-  {
+//-----------------------------------------------------------------------------
+t_tera_ranger_one_lidar::t_tera_ranger_one_lidar(byte motor_dir_pin, byte motor_step_pin, byte motor_enable_pin, byte infrared_pin)
+{
+	started = false;
+	infrared_read_first_time = false;
+
+	enable_pin = motor_enable_pin;
+
+	stepper = new AccelStepper(AccelStepper::DRIVER, motor_step_pin, motor_dir_pin);
+	stepper->setMaxSpeed(200);
+	stepper->setSpeed(200);
+	stepper->setAcceleration(100);
+
+	// reset pins
+	digitalWrite(motor_step_pin, LOW);
+	digitalWrite(motor_dir_pin, LOW);
+	digitalWrite(enable_pin, HIGH); // motor is disabled now
+
+	infrared_sensor.set_params(infrared_pin);
+}
+//-----------------------------------------------------------------------------
+void t_tera_ranger_one_lidar::start(void)
+{
+	digitalWrite(enable_pin, LOW); // turn motor on
+	stepper->move(400); //move num_steps
+	infrared_read_first_time = false;
+	request_for_distance_sent = false;
+
+	motor_position = 0;
+  started = true;
+}
+//-----------------------------------------------------------------------------
+void t_tera_ranger_one_lidar::run_motor(char *serial_out)
+{
+  serial_out[0] = 0;
+
+	if (started) {
     
-  }
-  //-----------------------------------------------------------------------------
-  void t_tera_ranger_one_lidar::start(void)
-  {
-    
-  }
-  //-----------------------------------------------------------------------------
-  void t_tera_ranger_one_lidar::run_motor(void)
-  {
-    
-  }
-  //-----------------------------------------------------------------------------
-  int t_tera_ranger_one_lidar::get_distance(void)
-  {
-    
-  }
+		if (request_for_distance_sent) {
+      Serial.write("3");
+			// no more steps until lidar sends a distance back
+			//if (tera_ranger_one.check_echo()) 
+			{
+				request_for_distance_sent = false;
+				// here I have to output a string
+				sprintf(serial_out, "L %d %d#", motor_position, 10); //tera_ranger_one.get_last_read_distance());
+			}
+		}
+		else {
+			// no request for distance yet
+			if (!infrared_sensor.get_value()) {
+        // infrared has detected an obstacle
+        Serial.write("4");
+				infrared_read_first_time = true;
+				motor_position = 0;
+			}
+			if (!infrared_read_first_time) {
+        Serial.write("1");
+				stepper->run();
+			}
+			else {
+        Serial.write("2");
+				long prev_dist_to_go = stepper->distanceToGo();
+        stepper->run();
+				long curr_dist_to_go = stepper->distanceToGo();
+
+				long traveled_distance = prev_dist_to_go - curr_dist_to_go;
+
+				if (traveled_distance) {
+					// now I have to read the distance
+					tera_ranger_one.trigger();
+
+					stepper->move(400); //move num_steps
+					request_for_distance_sent = true;
+					motor_position += traveled_distance;
+				}
+			}
+		}
+	}
+}
 //-----------------------------------------------------------------------------
--- jenny5-firmware/tera_ranger_one_lidar.h
+++ jenny5-firmware/tera_ranger_one_lidar.h
@@ -2,26 +2,29 @@
 #define TERA_RANGER_ONE_LIDAR_H
 
 #include "tera_ranger_one.h"
-#include "stepper_motor.h"
+#include "AccelStepper.h"
 #include "infrared_digital.h"
 
 class t_tera_ranger_one_lidar{
 
   private:
-  t_stepper_motor_controller *motor;
-  t_infrared_digital_sensor infrared;
+
+  byte enable_pin;
+  bool request_for_distance_sent;
+
+  AccelStepper *stepper;
+  t_infrared_digital_sensor infrared_sensor;
   t_tera_ranger_one_controller tera_ranger_one;
 
   bool started;
   bool infrared_read_first_time;
+  int motor_position;
 
 public:
   t_tera_ranger_one_lidar(byte motor_dir_pin, byte motor_step_pin, byte motor_enable_pin, byte infrared_pin);
   
-  void get_params(byte *motor_dir_pin, byte *motor_step_pin, byte *motor_enable_pin, byte *infrared_pin);
   void start(void);
-  void run_motor(void);
-  int get_distance(void);
+  void run_motor(char *serial_out);
 };
 
 #endif
